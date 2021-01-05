f63645ecfb596b29f43a4816364d2a7420e54c3a
Winckdev <apwinck@gmail.com>
2021-01-05 00:14:51 -0300

    fixes em arrendondamento

---
+++ index.js
@@ -0,0 +1,23 @@
+const port = document.getElementById("portfolio")
+const cont = document.getElementById("contato")
+const sob = document.getElementById("sobre")
+
+const game = document.getElementById("game")
+const jog = document.getElementById("jogador")
+
+const num1 = document.getElementById
+
+
+port.addEventListener("click", function(e){
+    const bings = document.getElementById("bings");
+    bings.style.color = "#f54363";
+})
+
+cont.addEventListener("click", function(e){
+    const bings = document.getElementById("bings");
+    bings.style.color = "#ffffff";
+})
+
+jog.style.width = "25px";
+jog.style.height = "25px";
+jog.style.backgroundColor = "#f54363";
